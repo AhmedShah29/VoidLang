bd34a58ff7aaffd1e09894fe290fb97f8777a677
Ahmed Shah <ahmedshawgi766@gmail.com>
2026-08-05 16:27:14 +0300

    Void lang CLI entery point

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import { lexer } from "./lexer.js";
+import { parser } from "./parser.js";
+
+const fileName = Bun.argv[2]
+
+// checks if ther is no file added or the file is not a .void file
+if (!fileName) { console.error("No file given to compile"); process.exit(1) }
+if (!fileName.endsWith(".void")) { console.error("Uknown file type"); process.exit(1) }
+
+const compile = async () => {
+  try {
+    const file = Bun.file(fileName)
+    const code = await file.text()
+
+    const tokens = lexer(code)
+    const ast = parser(tokens)
+    console.dir(ast, { depth: null, colors: true }); // AST printing
+    
+  } catch (err) {
+    console.error("Error has happend during compiling", err)
+    process.exit(1)
+  }
+}
+compile()
