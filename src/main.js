624944cfdac77458ba790dafb260500f70b35f8e
Ahmed Shah <ahmedshawgi766@gmail.com>
2026-08-08 18:19:55 +0300

    Fixed some file paths issues

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
+#!/usr/bin/env bun
+
 import { lexer } from "./lexer.js";
 import { parser } from "./parser.js";
 import { genrateCode } from "./codegen.js";
+import { resolve, basename } from "path"
 
 const fileName = Bun.argv[2]
 
@@ -8,20 +11,24 @@ const fileName = Bun.argv[2]
 if (!fileName) { console.error("No file given to compile"); process.exit(1) }
 if (!fileName.endsWith(".void")) { console.error("Uknown file type"); process.exit(1) }
 
+const fixedPath = resolve(process.cwd(), fileName)
+
 const compile = async () => {
   try {
-    const file = Bun.file(fileName)
+    const file = Bun.file(fixedPath)
     const code = await file.text()
 
     const tokens = lexer(code)
     const ast = parser(tokens)
     const genratedCode = genrateCode(ast)
 
-    Bun.write(`./builds/${fileName.replace(".void", ".js")}`, genratedCode, { createPath: true })
+    const cleanFileName = basename(fileName, ".void")
+
+    await Bun.write(`./builds/${cleanFileName}.js`, genratedCode, { createPath: true })
     console.log(`Successfully compiled ${fileName}`)
     
   } catch (err) {
-    console.error("Error has happend during compiling", err)
+    console.error("Error has happend during compiling\n", err.message)
     process.exit(1)
   }
 }
